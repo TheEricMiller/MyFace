4cf60618ea5dc38386695f4537e67dcb6b5eb71c
Eric Miller <Eric@TheEricMiller.com>
2013-05-03 14:05:11 -0400

    setup the state_machine for friend requests + added 'accepted' state and mailer + added 'pending' state + created pending_friends relationship

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -11,4 +11,14 @@ class UserNotifier < ActionMailer::Base
   		subject: "#{@user.first_name} wants to be a friend on MyFace"
   end
 
+  def friend_requested_accepted(user_friendship_id)
+  	user_friendship = UserFriendship.find(user_friendship_id)
+
+  	@user = user_friendship.user
+  	@friend = user_friendship.friend 
+
+  	mail to: @friend.email,
+  		subject: "#{@user.first_name} has accepted your friend request on MyFace"
+  end
+
 end
--- test/unit/user_friendship_test.rb
+++ test/unit/user_friendship_test.rb
@@ -13,7 +13,7 @@ class UserFriendshipTest < ActiveSupport::TestCase
 
 	test "that creating a friendship based on a user id and friend id works" do 
 		UserFriendship.create user_id: users(:user1fix).id, friend_id: users(:mike).id
-		assert users(:user1fix).friends.include?(users(:mike))
+		assert users(:user1fix).pending_friends.include?(users(:mike))
 	end
 
 
@@ -38,5 +38,46 @@ class UserFriendshipTest < ActiveSupport::TestCase
 			end
 		end
 	end
+
+	context "#accept!" do
+		setup do
+			@user_friendship = UserFriendship.create user: users(:user1fix), friend: users(:mike)
+		end
+
+		should "set the state to accepted" do
+			@user_friendship.accept!
+			assert_equal "accepted", @user_friendship.state
+		end
+
+		should "send an acceptance email" do
+			assert_difference 'ActionMailer::Base.deliveries.size', 1 do
+				@user_friendship.accept!
+			end
+		end
+
+		should "include the friend in the list of friends" do
+			@user_friendship.accept!
+			users(:user1fix).friends.reload
+			assert users(:user1fix).friends.include?(users(:mike))
+		end
+	end
+
+
+	context ".request" do
+		should "create two user friendships" do
+			assert_difference 'UserFriendship.count', 2 do
+				UserFriendship.request(users(:user1fix), users(:user2fix))
+			end
+		end
+
+		should "send a friend request email" do
+			assert_difference 'ActionMailer::Base.deliveries.size', 1 do
+				UserFriendship.request(users(:user1fix), users(:user2fix))
+			end
+		end
+	end
+
+
+
 
 end
