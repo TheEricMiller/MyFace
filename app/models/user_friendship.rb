63e977a150da7c3db86ff273209fa4de82c68d1e
Eric Miller <Eric@TheEricMiller.com>
2013-05-03 13:36:30 -0400

    installed 'state_machine' > Gemfile ~ gem 'state_machine' $ bundle $ rails g migration add_state_to_user_friendships > edit migration $ rake db:migrate
    $ rake db:test:prepare
    $ ruby -Itest test/unit/user_friendship_test.rb
    
    generated user_notifier mailer
    $ rails g mailer user_notifier

--- app/models/user_friendship.rb
+++ app/models/user_friendship.rb
@@ -5,4 +5,12 @@ class UserFriendship < ActiveRecord::Base
 
   attr_accessible :user, :friend, :user_id, :friend_id
 
+  state_machine :state, initial: :pending do
+  	
+  end
+
+  def send_request_email
+  	UserNotifier.friend_requested(id).deliver
+  end
+
 end
