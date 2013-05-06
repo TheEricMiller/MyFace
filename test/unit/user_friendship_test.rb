d39be3b7f9b9226d9c22ac4e22f669bc11741226
Eric Miller <Eric@TheEricMiller.com>
2013-05-06 17:48:31 -0400

    added new blocked state and tests for user_friendships

--- test/unit/user_friendship_test.rb
+++ test/unit/user_friendship_test.rb
@@ -143,5 +143,22 @@ class UserFriendshipTest < ActiveSupport::TestCase
 	end
 
 
+	context "#block!" do
+		setup do
+			@user_friendship = UserFriendship.request users(:user1fix), users(:user2fix)
+		end
+
+		should "set the state to blocked" do
+			@user_friendship.block!
+			assert_equal 'blocked', @user_friendship.state
+			assert_equal 'blocked', @user_friendship.mutual_friendship.state
+		end
+
+		should "not allow new requests once blocked" do
+			@user_friendship.block!
+			uf = UserFriendship.request users(:user1fix), users(:user2fix)
+			assert !uf.save
+		end
+	end
 
 end
